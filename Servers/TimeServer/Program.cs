f1816ca9dd454c5a249ff33996fce0dd0c4e7ebc
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-04-27 18:00:03 +0200

    Timeserver now remains active after connections

--- Servers/TimeServer/Program.cs
+++ Servers/TimeServer/Program.cs
@@ -12,33 +12,35 @@ namespace Servers.Time
         {
             Console.WriteLine("Starting server...");
 
-            // IPAddress address = IPAddress.Parse("172.0.0.1");
-            
             TcpListener server = new TcpListener(port);
 
             server.Start();
 
             Console.WriteLine("Server started...");
-            Console.WriteLine("Listening for connections...");
+
+            while (true)
+            {
+                Console.WriteLine("Listening for connections...");
             
-            var connectedClient = server.AcceptTcpClient();
+                var connectedClient = server.AcceptTcpClient();
 
-            Console.WriteLine("Connection accepted...");
+                Console.WriteLine("Connection accepted...");
 
-            var stream = connectedClient.GetStream();
+                var stream = connectedClient.GetStream();
 
-            byte[] p = new[] {(byte) 255, (byte) 165, (byte) 42};
-
-            Console.WriteLine("Sending bytes...");
+                Console.WriteLine("Sending bytes...");
             
-            stream.Write(Encoding.ASCII.GetBytes(DateTime.Now.ToString()));
+                stream.Write(Encoding.ASCII.GetBytes(DateTime.Now.ToString()));
 
-            Console.WriteLine("Bytes sent...");
+                Console.WriteLine("Bytes sent...");
+            
+            
+                Console.WriteLine("Closing down...");
 
-            Console.WriteLine("Closing down...");
-
-            stream.Close();
-            connectedClient.Close();
+                stream.Close();
+                connectedClient.Close();
+            }
+            
             server.Stop();
         }
     }
