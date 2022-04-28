5be4e209f8eb0261ae1aeb4670f06acd8034b508
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-04-28 11:54:32 +0200

    OpenWordMMO: Added error handling for long words.
    Also did some async training to "load" the bytearrays for the error messages on a separate thread. Even though it is not worth it in this case (The calculations are just really quick either way), I wanted to get some training with it. So I did it anyway.

--- Servers/OpenWord-MMO-Server/Program.cs
+++ Servers/OpenWord-MMO-Server/Program.cs
@@ -4,23 +4,50 @@ using System.Text;
 
 class Server
 {
-    private const int port = 4444;
-    
+    private const int Port = 4444;
+    private const byte MaximumWordLength = 20;
+
+    private static byte[] WordToLongErrorMessage;
+    private static byte[] MultipleWordsReceivedErrorMessage;
+
+    private static async Task CreateByteArraysForErrorMessages()
+    {
+        WordToLongErrorMessage =
+            Encoding.ASCII.GetBytes("Error! Sent word is to long! Please limit your words to a length of " +
+                                    MaximumWordLength + " letters!");
+        MultipleWordsReceivedErrorMessage =
+            Encoding.ASCII.GetBytes("Error! You are only allowed to send one word at a time!");
+    }
+
     static void Main()
     {
+        // I know there is no way it it worth it to put loading this small things in a separate thread like this. But, I
+        // wanted the training so I did it anyway.
+        var InitializationTask = CreateByteArraysForErrorMessages();
+        
         byte[] fullMessage = Array.Empty<byte>();
 
-        UdpClient udpClient = new UdpClient(port);
+        UdpClient udpClient = new UdpClient(Port);
 
         IPEndPoint remoteEndPoint = null;
-
-
         
-   
+        // Again, not something you should probably do in a case like this, but did just for training purposes.
+        InitializationTask.Wait();
         
         while (true)
         {
             var receivedBytes = udpClient.Receive(ref remoteEndPoint);
+            
+            Console.WriteLine($"Received {receivedBytes.Length} bytes.");
+            
+            // Check if the received message was to long.
+            if (receivedBytes.Length > MaximumWordLength)
+            {
+                udpClient.Send(WordToLongErrorMessage, WordToLongErrorMessage.Length, remoteEndPoint);
+                continue;
+            }
+
+
 
             Console.WriteLine(Encoding.ASCII.GetString(receivedBytes));
         }
