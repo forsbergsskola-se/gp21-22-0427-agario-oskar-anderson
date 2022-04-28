6535628f78e763b6fa01c16130ef34fb524ff761
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-04-28 10:21:09 +0200

    Timeclient now can communicate with server

--- Client/Agario/Assets/Scripts/TimeClient/RequestServerTime.cs
+++ Client/Agario/Assets/Scripts/TimeClient/RequestServerTime.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
 
 public class RequestServerTime : MonoBehaviour
 {
+    [SerializeField] private string ipAddress;
+    [SerializeField] private int port;
+    
     public void SendRequest()
     {
-        Debug.Log("Not implemented!");
+        TcpClient timeClient = new TcpClient(ipAddress, port);
+
+        var stream = timeClient.GetStream();
+
+        var buffer = new byte[22];
+
+        stream.Read(buffer, 0, 22);
+
+        var resultString = Encoding.ASCII.GetString(buffer);
+
+        Debug.Log(resultString);
     }
 }
