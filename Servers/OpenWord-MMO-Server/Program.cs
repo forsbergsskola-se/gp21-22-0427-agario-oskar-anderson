9c164df2dad9de0097cc95944a7c311832b263a9
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-04-28 11:23:11 +0200

    First working test of Udp clients.

--- Servers/OpenWord-MMO-Server/Program.cs
+++ Servers/OpenWord-MMO-Server/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    private const int port = 4444;
+    
+    static void Main()
+    {
+        byte[] fullMessage = Array.Empty<byte>();
+
+        UdpClient udpClient = new UdpClient(port);
+
+        IPEndPoint remoteEndPoint = null;
+
+
+        
+   
+        
+        while (true)
+        {
+            var receivedBytes = udpClient.Receive(ref remoteEndPoint);
+
+            Console.WriteLine(Encoding.ASCII.GetString(receivedBytes));
+        }
+        
+        udpClient.Close();
+    }
+}
