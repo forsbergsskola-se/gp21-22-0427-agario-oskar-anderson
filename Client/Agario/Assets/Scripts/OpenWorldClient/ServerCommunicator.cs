cc564c85873240650456a60639031ed171ff1d61
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-04-28 18:05:08 +0200

    Prepared scripts for OpenWordClient

---
+++ Client/Agario/Assets/Scripts/OpenWorldClient/ServerCommunicator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OpenWordClient
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+        public void Start()
+        {
+            EstablishConnection();
+        }
+
+        private void EstablishConnection()
+        {
+            
+        }
+
+        private void SendWord(string word)
+        {
+            
+        }
+
+        private string ReceiveWord()
+        {
+            
+        }
+    }
+}
