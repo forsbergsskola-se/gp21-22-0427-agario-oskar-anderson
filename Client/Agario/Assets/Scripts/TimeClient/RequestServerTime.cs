4dbb80c8e7817629bd03ad9983b21116a89e1da0
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-04-28 10:06:00 +0200

    TimeClient: Connected button to RequestTime script

---
+++ Client/Agario/Assets/Scripts/TimeClient/RequestServerTime.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RequestServerTime : MonoBehaviour
+{
+    public void SendRequest()
+    {
+        Debug.Log("Not implemented!");
+    }
+}
