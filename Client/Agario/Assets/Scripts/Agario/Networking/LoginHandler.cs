a36c9eaebbbcb91895041ec2c80648b01a27f307
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-05-04 19:02:24 +0200

    Progress update on client.
    Login request sending works, but receiving userdata is still broken.

--- Client/Agario/Assets/Scripts/Agario/Networking/LoginHandler.cs
+++ Client/Agario/Assets/Scripts/Agario/Networking/LoginHandler.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading;
 using UnityEngine;
@@ -16,43 +17,112 @@ namespace Agario.Networking
 
 
         public string ReturnMessage;
+        public string ReturnMessage2;
+        public string output;
         
-        public void ConnectToServer(string ipAddress, int port, string userName, Color color)
+        public void ConnectToServer(string ipAddress, int port, string userName, System.Drawing.Color color)
         {
             tcpClient = new TcpClient(ipAddress, port);
 
             var stream = tcpClient.GetStream();
 
-            
-            var nameBytes = Encoding.UTF8.GetBytes(userName);
-            var bytesToSend = new byte[nameBytes.Length + 1];
 
-            nameBytes.CopyTo(bytesToSend, 1);
-            
-            // stream.Write(new byte[]{0});
-            // stream.Write(Encoding.UTF8.GetBytes(userName));
-            stream.Write(bytesToSend);
-            
-            // TODO: We need to have this for the copyto method on the server to work. How do we fix this?
-            // stream.Close();
-            
-            stream = tcpClient.GetStream();
-            
-            MemoryStream memoryStream = new();
-            stream.CopyTo(memoryStream);
-            byte[] bytes = memoryStream.ToArray();
 
-            ReturnMessage = Encoding.UTF8.GetString(bytes);
+           
+            var streamWriter = new StreamWriter(stream);
+            // var options = new DataContractJsonSerializerSettings(){InludeFields = true};
+
+            var networkPackage = new NetworkPackage<User>(0, new User(color, userName));
+
+            networkPackage.Value = new User(color, userName);
+            
+            string JsonMessage = JsonUtility.ToJson(networkPackage);
+            ReturnMessage = JsonMessage;
+            streamWriter.WriteLine(JsonMessage);
+            streamWriter.Flush();
+
+            var streamReader = new StreamReader(stream);
+            string returnJsonMessage = streamReader.ReadLine();
+
+            var returnedPackage = JsonUtility.FromJson<NetworkPackage>(returnJsonMessage);
+
+            ReturnMessage2 = returnJsonMessage;
+            
+            if (returnedPackage.Id == 2)
+            {
+                var userData = JsonUtility.FromJson<NetworkPackage<UserData>>(returnJsonMessage).Value;
+                output =
+                    $"Welcome to the server {userData.UserName}{userData.id}";
+            }
+            else
+            {
+                output = "Returned package had incorrect id. " + returnedPackage.Id;
+            }
         }
 
         public void TestConnectionTEMP()
         {
-            new Thread(() => ConnectToServer("192.168.1.8", 25565, "Oskar", Color.black)).Start();
+            new Thread(() => ConnectToServer("192.168.1.8", 25565, "Oskar", System.Drawing.Color.Blue)).Start();
         }
 
         private void Start()
         {
             TestConnectionTEMP();
+        }
+    }
+    
+    
+    public class UserData
+    {
+        // TODO: Unitys stupid serializer doesn't work well with things that are totally fine for the C# one. 
+        public string UserName { get; private set; }
+        public System.Drawing.Color UserColor { get; private set; }
+        public readonly sbyte id;
+        private static sbyte nextId = 0;
+    
+    
+    
+        public UserData(string userName, System.Drawing.Color userColor)
+        {
+            id = nextId;
+            nextId++;
+            UserName = userName;
+            UserColor = userColor;
+        }
+    }
+
+    [Serializable]
+    public class NetworkPackage
+    {
+        public int Id;
+        
+        public NetworkPackage(int id)
+        {
+            Id = id;
+        }
+    }
+
+    [Serializable]
+    public class NetworkPackage<T> : NetworkPackage
+    {
+        public T Value;
+
+        public NetworkPackage(int Id, T value) : base(Id)
+        {
+            Value = value;
+        }
+    }
+
+    [Serializable]
+    public class User
+    {
+        public string UserName;
+        public System.Drawing.Color UserColor;
+
+        public User(System.Drawing.Color color, string userName = "Player")
+        {
+            UserName = userName;
+            UserColor = color;
         }
     }
 }
