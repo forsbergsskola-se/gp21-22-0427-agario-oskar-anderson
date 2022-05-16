6186aa7d9d80db656a8d55e31e59a4494b983f89
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-05-16 16:44:57 +0200

    Tweaked Size Gain from food.

--- Client/Agario/Assets/Scripts/Agario/Entities/Size.cs
+++ Client/Agario/Assets/Scripts/Agario/Entities/Size.cs
@@ -10,8 +10,8 @@ namespace Agario.Entities
 
         public void SetSizeFromScore(float score)
         {
-            float diameter = Mathf.Sqrt(score / Mathf.PI);
-            float scale = diameter + 1;
+            float radius = Mathf.Sqrt(score / Mathf.PI) / 2;
+            float scale = radius + 1;
             entityTransform.localScale = new Vector3(scale, scale, scale);
         }
     }
