6fd3e16fd4ec0a8e3054f01e11ac57e2a9cabfb8
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-05-17 09:55:02 +0200

    Created TNC interfaces and moved folder.

---
+++ Client/Agario/Assets/Scripts/Agario/TinyNetworkingConnection/Interfaces/IOutputLog.cs
@@ -0,0 +1,11 @@
+﻿namespace TNC.Interfaces
+{
+    public interface IOutputLog
+    {
+        /// <summary>
+        /// Print the input value to the preferred logging tool.
+        /// </summary>
+        /// <param name="text"></param>
+        public void PrintOutput(string text);
+    }
+}
